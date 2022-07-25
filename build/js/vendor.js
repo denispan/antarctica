835414832b434c48db1ff89b4a624a7cd0ab5603
denispan <8888670@gmail.com>
2022-07-25 10:23:16 +0300

    После тестирования

--- build/js/vendor.js
+++ build/js/vendor.js
@@ -4,32 +4,31 @@ function initialMap() {
 }
 
 function menuOpen() {
-  const header = document.querySelector('.header');
+  const header = document.querySelector('.header__container');
   const navMenu = document.querySelector('.main-nav');
   const navToggle = document.querySelector('.menu-toggle');
   const navItems = navMenu.querySelectorAll('a[href*="#"]');
   const body = document.body;
 
   function navMenuOpen() {
-    header.classList.add('header--menu-opened');
+    header.classList.add('header__container--menu-opened');
     navMenu.classList.add('main-nav--menu-opened');
     navToggle.classList.add('menu-toggle--opened');
     body.classList.add('scroll-off');
   }
 
   function navMenuClose() {
-    header.classList.remove('header--menu-opened');
+    header.classList.remove('header__container--menu-opened');
     navMenu.classList.remove('main-nav--menu-opened');
     navToggle.classList.remove('menu-toggle--opened');
     body.classList.remove('scroll-off');
   }
 
-
-  header.classList.remove('header--nojs');
+  header.classList.remove('header__container--nojs');
   navMenu.classList.remove('main-nav--nojs');
 
   navToggle.addEventListener('click', function () {
-    if (header.classList.contains('header--menu-opened')) {
+    if (header.classList.contains('header__container--menu-opened')) {
       navMenuClose();
     } else {
       navMenuOpen();
